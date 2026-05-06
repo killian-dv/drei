998d2170d4f7a6fa547d38f44b371a0a7e6d43a1
killian-dv <killian.david@etudiant.univ-reims.fr>
2026-05-06 23:08:09 +0200

    ✨: replace meshStandardMaterial with MeshReflectorMaterial for improved visual effects in Experience component

--- src/experience.tsx
+++ src/experience.tsx
@@ -1,6 +1,7 @@
 import {
   Float,
   Html,
+  MeshReflectorMaterial,
   OrbitControls,
   PivotControls,
   Text,
@@ -47,7 +48,13 @@ export const Experience = () => {
 
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
-        <meshStandardMaterial color="greenyellow" />
+        <MeshReflectorMaterial
+          blur={[1000, 1000]}
+          mixBlur={1}
+          resolution={512}
+          mirror={0.75}
+          color="greenyellow"
+        />
       </mesh>
 
       <Suspense fallback={null}>
